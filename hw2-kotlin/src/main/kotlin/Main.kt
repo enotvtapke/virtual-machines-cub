9ce80990146775fb3aaa0854e053890d195e60f5
enotvtapke <a.stupnikov05@gmail.com>
2025-10-24 02:52:55 +0200

    Add bytecode. Change main

--- hw2-kotlin/src/main/kotlin/Main.kt
+++ hw2-kotlin/src/main/kotlin/Main.kt
@@ -8,27 +8,20 @@ import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.readText
 
 fun main() {
-//    {
-//        val bytecode = Bytecode(Path.of("test001.bc"))
-//        println(bytecode)
-//        val instructions = generateSequence { bytecode.next() }
-//        println(instructions.joinToString("\n"))
-//    }()
-//
-//    val bytecode = Bytecode(Path.of("test002.bc"))
-//    val interpreter = Interpreter(bytecode, listOf(1, 2, 3, 4, 5))
-//    println(generateSequence { interpreter.interpret() }.toList())
-//    println(interpreter.output())
+    test("test*.lama")
+    test("Sort.lama")
+}
 
+private fun test(pattern: String) {
     val tests = Path("./regression")
-    println(tests.toAbsolutePath())
-    tests.listDirectoryEntries("Sort.lama").sorted().forEach { source ->
+    tests.listDirectoryEntries(pattern).sorted().forEach { source ->
         val filename = source.fileName.toString().removeSuffix(".lama")
-        val input = tests.resolve(Path("$filename.input")).readText().split("\n").filter{ it.isNotEmpty() }.map { it.toInt() }
+        val input =
+            tests.resolve(Path("$filename.input")).readText().split("\n").filter { it.isNotEmpty() }.map { it.toInt() }
         println("Testing $filename\n")
 
         var bytecode = Bytecode(tests.resolve(Path("$filename.bc")))
-        val instructions = generateSequence {
+        generateSequence {
             val pos = bytecode.code.position()
             val instr = bytecode.next()
             println("$pos: $instr")
@@ -37,14 +30,14 @@ fun main() {
         bytecode = Bytecode(tests.resolve(Path("$filename.bc")))
         val interpreter = Interpreter(bytecode, input)
         try {
-            while (interpreter.interpret() != null) { }
-//            generateSequence { interpreter.interpret() }.toList()
+            while (interpreter.interpret() != null) {
+            }
         } catch (e: Exception) {
             println(bytecode)
             println(interpreter.state)
             throw e
         }
-        println(interpreter.output())
+        println("\n" + interpreter.output())
         println("\n======================\n")
     }
 }
